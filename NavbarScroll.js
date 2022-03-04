c959ed439fad7fd8d73230e26de67b16fa9ea169
ArsonistD <39225423+arsonistd@users.noreply.github.com>
2022-03-04 11:59:50 -0500

    Update NavbarScroll.js

--- NavbarScroll.js
+++ NavbarScroll.js
@@ -1,11 +1,14 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
+var length = window.innerWidth
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.height = "75px";
-    document.getElementById("logo").style.height = "100px";
-    document.getElementById("logo").style.width = "100px";
+    if length < 700 {
+      document.getElementById("navbar").style.height = "75px";
+      document.getElementById("logo").style.height = "100px";
+      document.getElementById("logo").style.width = "100px";
+    }
   } else {
     document.getElementById("navbar").style.height = "175px";
     document.getElementById("logo").style.height = "150px";
