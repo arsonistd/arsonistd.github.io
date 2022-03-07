b123a6e2c3e099d851b961efa8cc19abcd0e18dc
ArsonistD <39225423+arsonistd@users.noreply.github.com>
2022-03-07 11:58:16 -0500

    remove blog

--- NavbarScroll.js
+++ NavbarScroll.js
@@ -1,17 +1,26 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
-var length = window.innerWidth
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    if length < 700 {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) { // Page is below top
+    if (window.innerWidth > 700) { //Page is above 700 width
       document.getElementById("navbar").style.height = "75px";
       document.getElementById("logo").style.height = "100px";
       document.getElementById("logo").style.width = "100px";
+    } else { //Page is below 700 width
+      document.getElementById("navbar").style.height = "200px";
+      document.getElementById("logo").style.height = "100px";
+      document.getElementById("logo").style.width = "100px";
     }
-  } else {
-    document.getElementById("navbar").style.height = "175px";
-    document.getElementById("logo").style.height = "150px";
-    document.getElementById("logo").style.width = "150px";
+  } else { // Page is at top
+    if (window.innerWidth > 700) { //Page is above 700 width
+      document.getElementById("navbar").style.height = "150px";
+      document.getElementById("logo").style.height = "150px";
+      document.getElementById("logo").style.width = "150px";
+    } else { //Page is below 700 width
+      document.getElementById("navbar").style.height = "300px";
+      document.getElementById("logo").style.height = "150px";
+      document.getElementById("logo").style.width = "150px";
+    }
   }
 }
